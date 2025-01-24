f8dc6e857c35da3249abc9bd9105658f994dc564
jacob157-rgb <jacobjokey@gmail.com>
2025-01-24 11:03:59 +0700

    Update: Adding WiFi Manager

--- NTPRTCP10.ino
+++ NTPRTCP10.ino
@@ -1,163 +1,131 @@
 /*
- @author Jacob JS
+  @author Jacob JS
 */
-
-#include <ESP8266WiFi.h>
-#include <NTPClient.h>
+#include <WiFiManager.h>  // https://github.com/tzapu/WiFiManager
+#include <NTPClient.h>    // https://github.com/arduino-libraries/NTPClient
 #include <WiFiUdp.h>
 #include <Wire.h>
-#include <RTClib.h>
-#include <DMDESP.h>
+#include <RTClib.h>       // https://github.com/adafruit/RTClib
+#include <DMDESP.h>       // https://github.com/farid1991/DMDESP
 #include <fonts/JAM11x28.h>
 
-const char *ssid     = "Redmi Note 12 Pro";
-const char *password = "42006709";
+// WiFiManager Setup
+WiFiManager wm;
 
+// NTPClient Setup
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org");
+
+// RTC Setup
 RTC_DS3231 rtc;
+
 // DMDESP Setup
-// #define Font EMSansSP8x16
 #define Font JAM11x28
-#define DISPLAYS_WIDE 2 // Kolom Panel
-#define DISPLAYS_HIGH 2 // Baris Panel
-DMDESP Disp(DISPLAYS_WIDE, DISPLAYS_HIGH);  // Jumlah Panel P10 yang digunakan (KOLOM,BARIS)
-byte Jam;
-byte Men;
+#define DISPLAYS_WIDE 2 // Number of columns of P10 panels
+#define DISPLAYS_HIGH 2 // Number of rows of P10 panels
+DMDESP Disp(DISPLAYS_WIDE, DISPLAYS_HIGH);  // Initialize DMDESP
+
+// Time Variables
+int Jam = 0; // Hours
+int Men = 0; // Minutes
 
 void setup() {
   // Serial Init
   Serial.begin(115200);
-
-  // Connect to Wi-Fi
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("\nWiFi connected!");
-
   // NTPClient Init
   timeClient.begin();
-  timeClient.setTimeOffset(3600 * 7); // Waktu Indonesia Barat
+  timeClient.setTimeOffset(3600 * 7); // Set to WIB (UTC+7)
+
+  // Reset WiFi settings for testing (optional)
+  wm.resetSettings();
+  wm.setClass("invert");
+  wm.setConfigPortalBlocking(false);
+  wm.setConfigPortalTimeout(60);
+
+  if (wm.autoConnect("AP-JamKantin", "kantin1938")) {
+    Serial.println("\nWiFi Connection Established!");
+  } else {
+    Serial.println("Config portal running.");
+  }
 
   // RTC Init
   if (!rtc.begin()) {
     Serial.println("RTC not detected. Using NTP time only.");
-    timeClient.update(); // Update waktu dari NTP
+    timeClient.update(); // Update time from NTP
     Jam = timeClient.getHours();
     Men = timeClient.getMinutes();
   } else {
-    // Update RTC from NTP if Wi-Fi is available
+    // Update RTC from NTP if WiFi is available
     if (WiFi.status() == WL_CONNECTED) {
-      Serial.println("Updating RTC from NTP...");
-      setRTCFromNTP(true);
+      updateRTC();
     } else if (rtc.lostPower()) {
-      Serial.println("RTC lost power and no Wi-Fi available. Unable to update time.");
+      Serial.println("RTC lost power and no WiFi available. Unable to update time.");
     }
   }
 
   // Initialize DMDESP
   Disp.start();
-  Disp.setBrightness(100); // Tingkat kecerahan
-  Disp.setFont(Font);      // Huruf
+  Disp.setBrightness(100); // Brightness level
+  Disp.setFont(Font);      // Set font
 }
 
-void setRTCFromNTP(bool updateRTC) {
+void updateRTC() {
   timeClient.update();
   time_t epochTime = timeClient.getEpochTime();
   struct tm *ptm = gmtime((time_t *)&epochTime);
   DateTime ntpTime = DateTime(ptm->tm_year + 1900, ptm->tm_mon + 1, ptm->tm_mday, ptm->tm_hour, ptm->tm_min, ptm->tm_sec);
-
-  // Print NTP time
-  Serial.print("NTP Time: ");
-  Serial.print(ntpTime.year());
-  Serial.print("-");
-  Serial.print(ntpTime.month());
-  Serial.print("-");
-  Serial.print(ntpTime.day());
-  Serial.print(" ");
-  Serial.print(ntpTime.hour());
-  Serial.print(":");
-  Serial.print(ntpTime.minute());
-  Serial.print(":");
-  Serial.println(ntpTime.second());
-
-  Jam = ntpTime.hour();
-  Men = ntpTime.minute();
-
-  // Check RTC vs NTP time difference
-  if (updateRTC) {
-    DateTime rtcTime = rtc.now();
-    long diff = (long)rtcTime.unixtime() - (long)ntpTime.unixtime();
-    diff = diff < 0 ? -diff : diff; // Absolute value
-
-    if (diff > 5) { // If time difference exceeds 5 seconds
-      Serial.println("RTC time difference too large. Updating RTC from NTP...");
-      rtc.adjust(ntpTime);
-      Serial.println("RTC updated successfully.");
-    } else {
-      Serial.println("RTC time is accurate. No update needed.");
-    }
-  }
+  rtc.adjust(ntpTime);
 }
 
 void loop() {
-  static unsigned long previousMillis = 0;
+  wm.process();
+  static unsigned long previousMillisBlink = 0;
+  static unsigned long previousMillisRTC = 0;
   static bool showColon = true;
 
   if (WiFi.status() == WL_CONNECTED) {
     // Periodically update RTC from NTP
-    static unsigned long lastUpdate = 0;
-    if (millis() - lastUpdate > 3600000) { // Update setiap 1 jam
+    if (millis() - previousMillisRTC > 600000) { // Update every 10 minutes
       Serial.println("Periodic RTC update from NTP...");
-      setRTCFromNTP(true);
-      lastUpdate = millis();
+      updateRTC();
+      previousMillisRTC = millis();
     }
   }
 
   if (rtc.begin()) {
+    // Read time from RTC
     DateTime now = rtc.now();
-
-    Serial.print("Current Date: ");
-    Serial.print(now.year());
-    Serial.print("-");
-    Serial.print(now.month());
-    Serial.print("-");
-    Serial.print(now.day());
-    Serial.print(" ");
-    Serial.print(now.hour());
-    Serial.print(":");
-    Serial.print(now.minute());
-    Serial.print(":");
-    Serial.println(now.second());
-
     Jam = now.hour();
     Men = now.minute();
   } else {
+    // If RTC not available, use NTP time
     Serial.println("RTC not detected. Using NTP time...");
     timeClient.update();
     Jam = timeClient.getHours();
     Men = timeClient.getMinutes();
   }
-  
-   unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= 500) {
-    previousMillis = currentMillis;
+
+  // // Restart ESP if time is 00:00
+  // if (Jam == 0 && Men == 0) {
+  //   Serial.println("Time is 00:00. Restarting ESP...");
+  //   ESP.restart();
+  // }
+
+  // Blink colon every 500 ms
+  if (millis() - previousMillisBlink >= 500) {
+    previousMillisBlink = millis();
     showColon = !showColon;
   }
 
+  // Format time display
   char isi[6];
   if (showColon) {
-    sprintf(isi, "%02d:%02d", Jam, Men); // Dengan titik dua
+    sprintf(isi, "%02d:%02d", Jam, Men); // With colon
   } else {
-    sprintf(isi, "%02d!%02d", Jam, Men); // Tanpa titik dua
+    sprintf(isi, "%02d!%02d", Jam, Men); // Without colon
   }
-
   
+  // Display time on DMDESP
   Disp.setFont(Font);
   Disp.drawText(2, 2, isi); 
   Disp.loop();
